29a20466eb9daba8dca347cac1434c14014356f8
Ferdinand E. Silva <ferdinand.silva@kitesystems.com>
2012-09-18 12:01:26 +0800

    add login block functionality

--- Idecorate/admin/views.py
+++ Idecorate/admin/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import HttpResponse, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import messages
+from admin.models import LoginLog
+from datetime import datetime, timedelta
 
 @staff_member_required
 def admin(request):
@@ -14,16 +16,21 @@ def admin_login(request):
 
 	if request.method == 'POST':
 
+		loginLog = LoginLog.objects.filter(created__gte=(datetime.now() - timedelta(minutes=5)))
+
 		username = request.POST['username']
 		password = request.POST['password']
-
-		print request.META['REMOTE_ADDR']
 
 		request.session['admin_username'] = username
 		request.session['admin_password'] = password
 		request.session['admin_login_post'] = True
 
 		user = authenticate(username=username, password=password)
+
+		if loginLog.count() >= 5:
+			request.session['showMessages'] = True
+			messages.error(request, _('Please try to login after 5 minutes'))
+			return redirect('admin')
 
 		if str(username).strip() != "" and str(password).strip() != "":
 			request.session['showMessages'] = True
@@ -41,11 +48,20 @@ def admin_login(request):
 				except KeyError:
 					pass
 
+				#Successfull login, delete all the log attempts
+				LoginLog.objects.filter(ip_address=request.META['REMOTE_ADDR']).delete()
+
 			else:
 				if "showMessages" in request.session:
 					messages.error(request, _('Sorry this account is disabled'))
 		else:
 			if "showMessages" in request.session:
+				#log ip address
+				loginLog = LoginLog()
+				loginLog.created = datetime.now()
+				loginLog.ip_address = request.META['REMOTE_ADDR']
+				loginLog.save()
+
 				messages.error(request, _('Sorry we could not verify your username and password'))
 
 	return redirect('admin')
