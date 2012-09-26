70f42b31edbcb3a3349d321b307bc157e29f462b
Ferdinand E. Silva <ferdinand.silva@kitesystems.com>
2012-09-26 11:33:08 +0800

    add dynamic menus to interface

--- Idecorate/interface/templatetags/idecorate_interface.py
+++ Idecorate/interface/templatetags/idecorate_interface.py
@@ -1,6 +1,6 @@
 from django import template
 from django.utils.safestring import mark_safe
-from menu.models import FooterCopyright
+from menu.models import FooterCopyright, InfoMenu, SiteMenu, FooterMenu
 
 register = template.Library()
 
@@ -13,3 +13,69 @@ def get_interface_info(infoType):
 		copyright = FooterCopyright.objects.get(id=1)
 
 		return mark_safe(copyright.copyright)
+
+	elif infoType == "footer_menu":
+		element = '<ul>'
+
+		footer_menus = FooterMenu.objects.filter(parent=None,deleted=False).order_by('order')
+
+		for menu in footer_menus:
+
+			element += '<li><a href="%s">%s</a></li>' % (menu.link, menu.name)
+
+		element += '</ul>'
+
+		return mark_safe(element)
+
+@register.filter
+def getInterfaceMenus(menuType):
+
+	if menuType == "info":
+		menus = InfoMenu.objects.filter(parent=None,deleted=False).order_by('order')
+	else:
+		menus = SiteMenu.objects.filter(parent=None,deleted=False).order_by('order')
+
+	return menuInterfaceRecursion(menus)
+
+def menuInterfaceRecursion(menus):
+
+	element = ""
+	needToOpen = True
+	css_class = ""
+	link = ""
+
+	for menu in menus:
+		if menu.parent is None:
+			if needToOpen:
+				element += '<ul class="dropdown clearfix">'
+				needToOpen = False
+
+		else:
+			if needToOpen:
+				element += '<ul>'
+				needToOpen = False
+
+		if menu.link == "":
+			css_class = ' class="nonLink"'
+			link = menu.name
+		else:
+			css_class = ''
+			link = '<a href="%s">%s</a>' % (menu.link, menu.name)
+
+		element += '<li%s>%s' % (css_class, link)
+
+		if menus.model == type(InfoMenu()):
+
+			sub_menus = InfoMenu.objects.filter(parent__id=menu.id,deleted=False).order_by('order')
+			element += menuInterfaceRecursion(sub_menus)
+
+		else:
+			sub_menus = SiteMenu.objects.filter(parent__id=menu.id,deleted=False).order_by('order')
+			element += menuInterfaceRecursion(sub_menus)
+
+		element +='</li>'
+
+	if needToOpen == False:
+		element += '</ul>'
+
+	return mark_safe(element)
