244c7d22f4ad3ba3619785d8640d2b3d7dc7051b
Ryan Angeles <ryan.angeles@kitesystems.com>
2012-10-09 10:38:12 +0800

    styleboard app

--- Idecorate/media/interface/js/jquery.app.js
+++ Idecorate/media/interface/js/jquery.app.js
@@ -34,10 +34,12 @@ $(document).ready(function () {
                 product_image.src = '/media/dummyproduct/large/img_default_'+product_id+'.jpg';
                 
                 img_wrapped = $(product_image).load(function(){
-                    $('<div class="product unselected"><img src="' + product_image.src + '" /></div>').appendTo('#canvas-wrap').css({
+                    $('<div class="product unselected selected"><img src="' + product_image.src + '" /></div>').appendTo('#canvas-wrap').css({
                         top : e.pageY-$('#canvas-wrap').offset().top-product_image.height/2,
                         left :e.pageX-$('#canvas-wrap').offset().left-product_image.width/2,
                     });
+
+                    $('.handles').css({display: 'block'});
                 });
 			}
 		}
