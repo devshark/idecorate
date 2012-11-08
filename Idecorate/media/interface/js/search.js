eca5840308b84664d8edb79beec65aaec9c97d12
Chejaerubin Mora <chejaerubin@kite-dev-chejae-m.(none)>
2012-11-08 11:37:32 +0800

    search update

---
+++ Idecorate/media/interface/js/search.js
@@ -0,0 +1,20 @@
+$.widget( "custom.catcomplete", $.ui.autocomplete, {
+    _renderMenu: function( ul, items ) {
+        var that = this,
+            currentCategory = "";
+        $.each( items, function( index, item ) {
+            if ( item.category != currentCategory ) {
+                ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+                currentCategory = item.category;
+            }            
+            that._renderItemData( ul, item );
+        });
+    }
+});
+
+$(function() {     
+    $( "#search" ).catcomplete({
+        delay: 0,
+        source: SEARCH_SUGGESTION_URL
+    });
+});
