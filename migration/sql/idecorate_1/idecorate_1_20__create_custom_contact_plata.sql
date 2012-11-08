c8f8024995917fe820f9a1810d089138c5ce7ed7
Ferdinand E. Silva <ferdinand.silva@kitesystems.com>
2012-11-08 17:26:34 +0800

    add sql

---
+++ migration/sql/idecorate_1/idecorate_1_20__create_custom_contact_plata.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `cart_contact` (
+    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `user_id` integer NOT NULL UNIQUE,
+    `first_name` varchar(100) NOT NULL,
+    `last_name` varchar(100) NOT NULL,
+    `address` longtext NOT NULL,
+    `address2` longtext NOT NULL,
+    `state` varchar(100) NOT NULL,
+    `zip_code` varchar(50) NOT NULL,
+    `city` varchar(100) NOT NULL,
+    `shipping_same_as_billing` bool NOT NULL,
+    `currency` varchar(3) NOT NULL
+)
+;
+ALTER TABLE `cart_contact` ADD CONSTRAINT `user_id_refs_id_1a381a8b` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+COMMIT;
+
