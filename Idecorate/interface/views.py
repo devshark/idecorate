a4845d54b5fdb9d4f09dbcf46d1a2320c4b1501c
Chejaerubin Mora <chejaerubin@kite-dev-chejae-m.(none)>
2012-11-09 17:32:19 +0800

    search debug update

--- Idecorate/interface/views.py
+++ Idecorate/interface/views.py
@@ -427,7 +427,8 @@ def search_products(request):
 
 		if cat_id != '0':
 			cat_ids = get_cat_ids(cat_id)
-			product_list = Product.objects.filter(categories__id__in=cat_ids, is_active=True, is_deleted=False)			
+			product_list = Product.objects.filter(categories__id__in=cat_ids, is_active=True, is_deleted=False)	
+			print product_list.query		
 			product_list = product_list.order_by('ordering')		
 		else:
 			keywords = search_keyword.split(' ')
