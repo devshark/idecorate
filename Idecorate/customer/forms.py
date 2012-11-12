cdb6ab0e94dabe28c67534c88b8ced59b6bd480a
Chejaerubin Mora <chejaerubin@kite-dev-chejae-m.(none)>
2012-11-12 15:58:33 +0800

    login signup update

--- Idecorate/customer/forms.py
+++ Idecorate/customer/forms.py
@@ -6,3 +6,20 @@ from django.conf import settings
 class LoginForm(forms.Form):	
 	username = forms.EmailField(label=_("Enter Email"), error_messages={'required':_('Enter a valid email.')})
 	password = forms.CharField( label=_("Enter Password"), widget=forms.PasswordInput, error_messages={'required':_('Enter a valid Password.')})
+
+class SignupForm(forms.Form):
+	nickname = forms.CharField( label=_("Enter Nickname"))
+	email = forms.EmailField(label=_("Enter Email"), error_messages={'required':_('Enter a valid email.')})
+	password = forms.CharField( label=_("Enter Password"), widget=forms.PasswordInput, error_messages={'required':_('Enter a valid Password.')})
+	confirm_password = forms.CharField( label=_("Enter Password Again"), widget=forms.PasswordInput, error_messages={'required':_('Enter a valid Password.')})
+
+	def clean_confirm_password(self):
+		try:
+			password = self.data['password']
+			confirm_password = self.cleaned_data['confirm_password']            
+			if password:
+				if password != confirm_password:
+					raise forms.ValidationError(_("Confirm Password not match to Password."))
+			return confirm_password
+		except MultiValueDictKeyError as e:
+			return ""
