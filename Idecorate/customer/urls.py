093442f2d3ddcdc36e5e75606b3684945b57a4de
Chejaerubin Mora <chejaerubin@kite-dev-chejae-m.(none)>
2012-11-20 14:53:12 +0800

    save styleboard update and view styleboard initial changes

--- Idecorate/customer/urls.py
+++ Idecorate/customer/urls.py
@@ -6,5 +6,6 @@ urlpatterns =  patterns('customer.views',
     url(r'^logout/$', 'customer_logout', {}, name='logout'),
     url(r'^forgot/$', 'forgot_password', {}, name='forgot_password'),
     url(r'^profile/$', 'profile', {}, name='profile'),
-    url(r'^styleboard/save/$', 'save_styleboard', {}, name='save_styleboard'),    
+    url(r'^styleboard/save/$', 'save_styleboard', {}, name='save_styleboard'),
+    url(r'^styleboard/view/$', 'styleboard_view', {}, name='styleboard_view'),  
 )
