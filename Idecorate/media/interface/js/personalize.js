d94c9e6a60444341ac425086138ef2e0c4b7abe0
Chejaerubin Mora <chejaerubin@kite-dev-chejae-m.(none)>
2012-12-04 10:44:01 +0800

    personalize styleboard view

--- Idecorate/media/interface/js/personalize.js
+++ Idecorate/media/interface/js/personalize.js
@@ -4,12 +4,31 @@ $(document).ready(function(){
 
 function populate_save_styleboard(){
 	if (PERSONALIZE_ITEM != ''){
-		var item = eval(PERSONALIZE_ITEM);	
-		$.each(item, function(i,v){
-			//console.log(v.img[0].a)
-			// var elm = $('<div />');
-			// elm.attr('_angle',v.angle);
-			// elm.addClass('');
+		var item = eval(PERSONALIZE_ITEM);
+		$.each(item, function(i,v){			
+			var elm = $('<div />');
+			elm.attr('_angle',v.angle);
+			elm.addClass(v._type);
+			if (v._type!='product')
+				elm.addClass('embellishment');
+			elm.addClass('unselected');
+			elm.addClass('ui-draggable');
+			if (v.uid)
+				elm.attr('_uid',v.uid);
+			elm.attr('style',v.style);
+			elm.attr('_handle',v.handle);
+			elm.attr('_opacity',v.opacity);
+			elm.attr('def_qty',v.def_qty);
+			elm.attr('gst_tb',v.gst_tb)
+
+			var img = $('<img />');
+			img.attr('src',v.img[0].src);
+			img.attr('style',v.img[0].style);
+			img.attr('_nb',v.img[0].nb);
+			img.attr('_wb',v.img[0].wb);
+			img.appendTo(elm);
+			elm.appendTo('#canvas');
+			objCounter++;
 		});
 	}
 }
