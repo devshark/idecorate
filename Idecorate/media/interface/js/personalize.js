e0b0e3dd4406669ae04129a55b74a97d8d988633
Ryan Angeles <ryan.angeles@kitesystems.com>
2012-12-05 15:46:02 +0800

    centering styleboard

--- Idecorate/media/interface/js/personalize.js
+++ Idecorate/media/interface/js/personalize.js
@@ -61,39 +61,89 @@ function populate_save_styleboard(){
 }
 
 function make_center(){
-	var array_x = [];
-	var array_y = [];
-	var canvas_height = $('#canvas').height();
-	var canvas_width = $('#canvas').width();
-
-	var canvas_y = canvas_height/2;
-	var canvas_x = canvas_width/2;
+	var array_x = {};
+	var array_y = {};
 
 	$('#canvas .unselected').each(function(){
-		var h = $(this).height();
-		var w = $(this).width();
-
-
-		var hy = h/2;
-		var hx = w/2;
-
-		array_y.push($(this).css('top'));
-		array_x.push($(this).css('left'));
+		array_x[$(this).attr('_uid')] = parseFloat($(this).css('left'));
+		array_y[$(this).attr('_uid')] = parseFloat($(this).css('top'));
+		console.log($(this).width());
 	});
 
-	getCenter(array_x);
-	getCenter(array_y);
+	console.log(array_x);
+	console.log(array_y);
+
+	var raw_w = getCenter(array_x, 'left');
+	var raw_h = getCenter(array_y, 'top');
+	var bb_w = (raw_w.max - raw_w.min)/2;
+	var bb_h = (raw_h.max - raw_h.min)/2;
+	bb_w = raw_w.min+bb_w;
+	bb_h = raw_h.min+bb_h;
+
+	var ctr_diff = canvas_bb_ctr_diff(bb_h,bb_w);
+
+	// $('#canvas .unselected').each(function(){
+	// 	$(this).css({
+	// 		top:parseFloat($(this).css('top'))+ctr_diff.y,
+	// 		left:parseFloat($(this).css('left'))+ctr_diff.x
+	// 	});
+	// });
+
 }
 
-function getCenter(array) {
+function canvas_bb_ctr_diff(bbH, bbW){
+	var ctr_diff 	= {}
+	var canvas_ctr 	= {};
+	var bb_ctr 		= {'x':bbW, 'y':bbH};
+	canvas_ctr['x'] = $('#canvas').width()/2;
+	canvas_ctr['y'] = $('#canvas').height()/2;
 
-	//sort the array first
-	array = array.sort();
-	//get the index
-	var ind = array.length / 2;
-	ind = parseInt(Math.round(ind));
+	console.log(canvas_ctr);
+	console.log(bb_ctr);
 	
-	return array[ind - 1];
+	$('<div/>').css({
+		top : bbH,
+		left: bbW,
+		width: 5,
+		height: 5,
+		position: 'absolute',
+		backgroundColor: '#333',
+		zIndex: 10
+	}).appendTo('#canvas');
+
+	$('<div/>').css({
+		top : canvas_ctr.y,
+		left: canvas_ctr.x,
+		width: 5,
+		height: 5,
+		position: 'absolute',
+		backgroundColor: '#333',
+		zIndex: 10
+	}).appendTo('#canvas');
+
+	ctr_diff['x'] = canvas_ctr.x - bb_ctr.x;
+	ctr_diff['y'] = canvas_ctr.y - bb_ctr.y;
+
+	return ctr_diff;
+}
+
+function getCenter(array,pos) {
+	var coor = {};
+	var arr = Object.keys( array ).map(function ( key ) { return array[key]; });
+	var min = Math.min.apply( null, arr );
+	var max = Math.max.apply( null, arr );
+
+	coor['min'] = min;
+	$.each(array, function(i,val){
+		if(array[i] == max && pos == 'left'){
+			console.log($('.unselected[_uid="'+i+'"]').width());
+			coor['max'] = max + $('.unselected[_uid="'+i+'"]').width();
+		}else if(array[i] == max && pos == 'top'){
+			coor['max'] = max + $('.unselected[_uid="'+i+'"]').height();
+		}
+	});
+	console.log(coor);
+	return coor;
 }
 
 function get_cart_items(){
