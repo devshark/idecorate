772a7e7a5374f764b005279d7003ca1db5dca7e9
Chejaerubin Mora <chejaerubin@kite-dev-chejae-m.(none)>
2012-12-06 10:21:52 +0800

    personalize styleboard update

--- Idecorate/media/interface/js/personalize.js
+++ Idecorate/media/interface/js/personalize.js
@@ -19,6 +19,11 @@ function populate_save_styleboard(){
 			if (v.uid)
 				elm.attr('_uid',v.uid);
 			elm.attr('style',v.style);
+
+			elm.css('filter','none');
+	        elm.css('msfilter','none');
+	        elm.css('-ms-filter','none');
+
 			elm.attr('_handle',v.handle);
 			elm.attr('_opacity',v.opacity);
 			elm.attr('def_qty',v.def_qty);
@@ -55,10 +60,7 @@ function populate_save_styleboard(){
 			img.attr('_nb',v.img[0].nb);
 			img.attr('_wb',v.img[0].wb);
 			img.appendTo(elm);
-			elm.appendTo('#canvas');
-			elm.css('filter','none');
-	        elm.css('msfilter','none');
-	        elm.css('-ms-filter','none');
+			elm.appendTo('#canvas');			
 			objCounter++;			
 		});
 		setTimeout(make_center,0);
