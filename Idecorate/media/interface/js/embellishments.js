3bcc51253f94ce510987040883dc82c59ea84831
Chejaerubin Mora <chejaerubin@kite-dev-chejae-m.(none)>
2012-12-07 10:38:24 +0800

    ie7 bug fixed

--- Idecorate/media/interface/js/embellishments.js
+++ Idecorate/media/interface/js/embellishments.js
@@ -286,18 +286,20 @@ function get_embellishment_items(){
             emb_num_pages = response_data.num_pages;            
             $.each(data,function(i,v){
                 var id = v.pk;
-                var img_src_url = v.model == 'admin.embellishments'?EMB_IMG_GEN_URL+'?embellishment_id='+id+'&embellishment_color=000000000&embellishment_thumbnail=1&embellishment_size=100':TEXT_IMG_GEN_URL+'?font_size=100&font_text=Abc&font_color=000000000&font_id='+id+'&font_thumbnail=1';
-                var a = $('<a />');
-                a.attr('id','emb-'+id);
-                a.attr('_type',response_data.type);
-                a.addClass('thumb');
-                a.addClass('draggable');
-                a.addClass('hidden');
-                a.addClass('em');
-                var img = $('<img />');
-                img.attr('src',img_src_url);
-                img.appendTo(a);
-                a.appendTo('#embelishments-list-wrap .emItem');
+                if ($('#emb-'+id).length == 0){                    
+                    var img_src_url = v.model == 'admin.embellishments'?EMB_IMG_GEN_URL+'?embellishment_id='+id+'&embellishment_color=000000000&embellishment_thumbnail=1&embellishment_size=100':TEXT_IMG_GEN_URL+'?font_size=100&font_text=Abc&font_color=000000000&font_id='+id+'&font_thumbnail=1';
+                    var a = $('<a />');
+                    a.attr('id','emb-'+id);
+                    a.attr('_type',response_data.type);
+                    a.addClass('thumb');
+                    a.addClass('draggable');
+                    a.addClass('hidden');
+                    a.addClass('em');
+                    var img = $('<img />');
+                    img.attr('src',img_src_url);
+                    img.appendTo(a);
+                    a.appendTo('#embelishments-list-wrap .emItem');
+                }                
             });
             manage_embellishment_pagination();
             $('#embelishments-list-wrap .emCat').hide();
@@ -423,6 +425,17 @@ function manage_embellishment_resize(){
 function emb_remove_overlay(){
     if( $('.embellishment-overlay').length > 0 )
         $('.embellishment-overlay').remove();
+
+    if($.browser.msie && $.browser.version==7.0){
+        var emb = [];
+        $('#embelishments-list-wrap .emItem a').each(function(i,v){            
+            if ($.inArray($(this).attr('id'),emb)!=-1){
+                $(this).remove();
+            } else {
+                emb.push($(this).attr('id'));
+            }            
+        });
+    }
 }
 /* embellishment upload */
 function gen_uuid() {
