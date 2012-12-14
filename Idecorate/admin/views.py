edb26a566e0fb20f9e5462fad2a789e1ef1d885b
Ryan Angeles <ryan.angeles@kitesystems.com>
2012-12-14 16:05:50 +0800

    template management

--- Idecorate/admin/views.py
+++ Idecorate/admin/views.py
@@ -1714,6 +1714,7 @@ def admin_edit_user(request):
 @staff_member_required
 def manage_template(request):
     info = {}
+    info['text_items'] = TextFonts.objects.filter(is_active=True, is_deleted=False)
     return render_to_response('admin/manage_template.html',info,RequestContext(request))
 
 @staff_member_required
