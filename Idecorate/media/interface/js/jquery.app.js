d8089b792f04db7f5040cd186e913bd1c7be513a
Ferdinand E. Silva <ferdinand.silva@kitesystems.com>
2012-12-19 08:18:27 +0800

    fix remove cart

--- Idecorate/media/interface/js/jquery.app.js
+++ Idecorate/media/interface/js/jquery.app.js
@@ -651,6 +651,8 @@ function box_droppable(){
                         data: {product_id: uid},
                         async:   false,
                         success: function(data){
+                            //console.log($(_this).find('img').attr('_uid'));
+                            var currentProd = $(_this).find('img').attr('_uid');
                             var img_src     = '/'+_img_src+data.original_image;
                             var img_w_bg    = data.original_image;
                             var img_wo_bg   = data.no_background;
@@ -669,6 +671,10 @@ function box_droppable(){
                                         _width   : this_width,
                                         _height  : this_height
                                     });
+
+                            if(currentProd) {
+                                remove_from_cart(parseInt(currentProd,10));
+                            }
 
                             $(_this).html(object[0]);
                         },
