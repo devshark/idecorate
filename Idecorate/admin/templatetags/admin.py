6f9c51ec18409399f24368516bac04b2dcdc116c
Chejaerubin Mora <chejaerubin@kite-dev-chejae-m.(none)>
2012-12-20 11:19:52 +0800

    header update

--- Idecorate/admin/templatetags/admin.py
+++ Idecorate/admin/templatetags/admin.py
@@ -522,6 +522,8 @@ def getProductDetail(product,what):
 
 @register.filter
 def currency(dollars):
+	ls = dollars.split(',')
+	dollars = ''.join(ls)
 	dollars = round(float(dollars), 2)
 	return "%s%s" % (intcomma(int(dollars)), ("%0.2f" % dollars)[-3:])
 
