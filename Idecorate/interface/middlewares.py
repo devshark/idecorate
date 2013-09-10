0feed8db8a3f08f6831e8256f8e3760d29e29a8e
Mark Allan B Meriales <mark.meriales@kite-dev-markallan-m.(none)>
2013-09-10 11:26:47 +0800

    wishlist on login/signup

--- Idecorate/interface/middlewares.py
+++ Idecorate/interface/middlewares.py
@@ -6,18 +6,10 @@ class InterfaceMiddleware(object):
         sessionid = request.session.get('sessionid', False)
         if not sessionid:
             sessionid = generate_unique_id()
-
-        # get wishlist added before logging in
-        # add this to customer login
-        """
-        wishlist_session = request.session.get('wishlist_session', False)
-        if wishlist_session:            
-            if request.user.is_authenticated():
-                wishlists = WishList.objects.filter(sessionid=wishlist_session)
-                for wishlist in wishlists:
-                    wishlist.user = request.user
-                    wishlist.save()
-                    del request.session['wishlist_session']
-        """
-
         request.session['sessionid'] = sessionid
+        
+        if not request.user.is_authenticated():
+            wishlist_session_id = request.session.get('wishlist_session_id', False)
+            if not wishlist_session_id:
+                request.session['wishlist_session_id'] = sessionid
+
